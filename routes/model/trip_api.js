1556ca9b03df88affb78a41b21e31a183e0d43f7
thierry larché <tlarche@gmail.com>
2019-03-03 16:49:44 +0100

    Test

--- routes/model/trip_api.js
+++ routes/model/trip_api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
+/// $$$ TLA
 // __ TRIP SCHEMA ________________________________________
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const tripSchema = mongoose.Schema({
